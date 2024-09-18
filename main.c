1f3233599d4b48ad72ea6b5cec23536407eba5bc
Fabscii <129980914+FabianSchieder@users.noreply.github.com>
2024-09-18 08:40:39 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Handy
+{
+    char MARKE[20];
+    int PREIS;
+};
+
+int main(void)
+{
+    struct Handy HANDY1;
+    strcpy(HANDY1.MARKE, "SAMSUNG");
+    HANDY1.PREIS = 999;
+
+return 0;
+}
