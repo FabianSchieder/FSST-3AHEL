42f0200d46abad3f9f0b98d7a3721bcf6d9d9052
Fabscii <129980914+FabianSchieder@users.noreply.github.com>
2024-10-11 12:06:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-#include <string.h>
-
-struct Handy
-{
-    char MARKE[20];
-    int PREIS;
-};
-
-int main(void)
-{
-    struct Handy HANDY1;
-    strcpy(HANDY1.MARKE, "SAMSUNG");
-    HANDY1.PREIS = 999;
-
-return 0;
-}
+h = wert%5
